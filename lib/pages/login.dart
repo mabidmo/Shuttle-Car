140e3630e3647dce9dd71ff05a3b97bf29741158
MSI\mabid <mabidmurtadho@gmail.com>
2024-03-19 17:01:11 +0700

    Login & SignUp UI

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shuttle_car/pages/signup.dart';
 
 class login extends StatefulWidget {
   const login({super.key});
@@ -14,85 +17,162 @@ class _loginState extends State<login> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+        padding: EdgeInsets.fromLTRB(28, 0, 28, 0),
         child: SafeArea(
             child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image(
-                    image: AssetImage('images/Logo Apk.png'),
-                    width: 200,
+          child: SingleChildScrollView(
+            // reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('images/Logo Apk.png'),
+                      width: 200,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 25),
+                ),
+                Text(
+                  'Welcome Back',
+                  style: TextStyle(fontSize: 15, fontFamily: 'Roboto'),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    labelText: 'Email',
+                    hintText: 'Email UISI',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14)),
                   ),
-                ],
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Login',
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(
-                'Welcome Back',
-                style: TextStyle(fontSize: 12, fontFamily: 'Roboto'),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  labelText: 'Email',
-                  hintText: 'Email UISI',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              TextField(
-                obscureText: !_showPassword,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.lock),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _showPassword = !_showPassword;
-                        });
-                      },
-                      icon: Icon(_showPassword
-                          ? Icons.visibility
-                          : Icons.visibility_off)),
-                  labelText: 'Password',
-                  hintText: 'Password',
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
+                SizedBox(
+                  height: 10,
                 ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Container(
-                width: 360,
-                height: 50,
-                child: ElevatedButton(
+                TextField(
+                  obscureText: !_showPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
+                        },
+                        icon: Icon(_showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off)),
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14)),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 360,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(Colors.red),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(14)))),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text("Or Sign Up With"),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 360,
+                  height: 50,
+                  child: OutlinedButton.icon(
                     onPressed: () {},
                     style: ButtonStyle(
-                        backgroundColor: MaterialStatePropertyAll(Colors.red),
-                        shape: MaterialStatePropertyAll(RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20)))),
-                    child: Text(
-                      'Login',
-                      style: TextStyle(color: Colors.white),
-                    )),
-              )
-            ],
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                      ),
+                    ),
+                    icon: Icon(
+                      FontAwesomeIcons.google,
+                      color: Colors.black,
+                    ),
+                    label: Text(
+                      'Login With Google',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Tidak punya akun?"),
+                    TextButton(
+                      onPressed: () {
+                        // Action when Sign Up button is pressed
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => signup()),
+                        );
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         )),
       ),
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shuttle_car/pages/login.dart';
 
 class signup extends StatefulWidget {
   const signup({super.key});
@@ -8,8 +10,193 @@ class signup extends StatefulWidget {
 }
 
 class _signupState extends State<signup> {
+  bool _showPassword = false;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(28, 0, 28, 0),
+        child: SafeArea(
+            child: Center(
+          child: SingleChildScrollView(
+            // reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image(
+                      image: AssetImage('images/Logo Apk.png'),
+                      width: 200,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'Sign Up',
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(
+                  'Create Account',
+                  style: TextStyle(fontSize: 13),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    labelText: 'Email',
+                    hintText: 'Email UISI',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14)),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  obscureText: !_showPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
+                        },
+                        icon: Icon(_showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off)),
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14)),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  obscureText: !_showPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _showPassword = !_showPassword;
+                          });
+                        },
+                        icon: Icon(_showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off)),
+                    labelText: 'Re-Password',
+                    hintText: 'Ketik ulang password',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(14)),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 360,
+                  height: 50,
+                  child: ElevatedButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(Colors.red),
+                          shape: MaterialStatePropertyAll(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(14)))),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text("Or Sign Up With"),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Colors.black,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 360,
+                  height: 50,
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(14),
+                        ),
+                      ),
+                    ),
+                    icon: Icon(
+                      FontAwesomeIcons.google,
+                      color: Colors.black,
+                    ),
+                    label: Text(
+                      'Login With Google',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Saya mempunyai account"),
+                    TextButton(
+                      onPressed: () {
+                        // Action when Sign Up button is pressed
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => login()),
+                        );
+                      },
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )),
+      ),
+    );
   }
 }
